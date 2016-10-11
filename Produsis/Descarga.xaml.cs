03823e4317a723768112619aad4f068df0d19f0e
ThaisMap <thais.marpi@gmail.com>
2016-10-10 21:36:50 -0300

    More little tweaks

--- Produsis/Descarga.xaml.cs
+++ Produsis/Descarga.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BLL;
+using ProdusisBD;
 
 namespace GUI
 {
@@ -22,21 +24,33 @@ namespace GUI
     public partial class Descarga : UserControl
     {
         public FuncionariosTag FuncionarioSelecionado;
+
         public Descarga()
         {
             InitializeComponent();
             Funcionario = new ObservableCollection<GUI.FuncionariosTag>();
-            Funcionario.Add(new FuncionariosTag { Nome = "Paola Oliveira", Tag = CriaChipTag("Paola Oliveira") });
+
+            //Acessando BD
+            FuncionarioBLL f = new FuncionarioBLL();
+            foreach (string func in f.carregaFuncionarios())
+            {
+                Funcionario.Add(new FuncionariosTag(func, CriaChipTag(func)));
+            }
+
+            //Sem acessar BD
+            /* Funcionario.Add(new FuncionariosTag { Nome = "Paola Oliveira", Tag = CriaChipTag("Paola Oliveira") });
             Funcionario.Add(new FuncionariosTag { Nome = "Jose Silva", Tag = CriaChipTag("Jose Silva") });
             Funcionario.Add(new FuncionariosTag { Nome = "Astolfo Mexicano", Tag = CriaChipTag("Astolfo Mexicano") });
             Funcionario.Add(new FuncionariosTag { Nome = "Dutra Mexicano", Tag = CriaChipTag("Dutra Mexicano") });
             Funcionario.Add(new FuncionariosTag { Nome = "Loconauta Mexicano Gonzalez", Tag = CriaChipTag("Astolfo Zicado") });
+            */
 
             //ListaDeFuncionarios.ItemsSource = Funcionario;
             CBFuncionario.ItemsSource = Funcionario;
         }
 
         public ObservableCollection<FuncionariosTag> Funcionario { get; set; }
+
         public static string CriaChipTag(string Nome)
         {
             string[] PrimeirosNomes = new string[7];
@@ -46,6 +60,7 @@ namespace GUI
 
         private void ChipEx_DeleteClick(object sender, RoutedEventArgs e)
         {
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -56,7 +71,7 @@ namespace GUI
         private void CBFuncionario_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             FuncionariosTag Funcionario = (FuncionariosTag)CBFuncionario.SelectedItem;
-            FuncionarioSelecionado = new FuncionariosTag { Nome = Funcionario.Nome , Tag = Funcionario.Tag};
+            FuncionarioSelecionado = new FuncionariosTag { Nome = Funcionario.Nome, Tag = Funcionario.Tag };
         }
     }
 }
